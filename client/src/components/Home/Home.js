172c904a4a7846c0187fab3d73ff88a711f37096
Samyakkumar <samyakkumar@gmail.com>
2019-10-27 10:55:46 -0400

    Add home button

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,14 +1,26 @@
 import React, {useState, useEffect} from 'react';
 import {useCookies} from 'react-cookie';
+import {Button} from 'semantic-ui-react';
 
 function Home() {
     const [uuid, setUuid] = useState("null");
-    useEffect(() => {
+    const [url, setUrl] = useState("null");
+    const [gotUrl, setGotUrl] = useState(false);
+    function clickHandler() {
         fetch("/api/getUserId/", {
             method: "GET"
-        }).then(dat => dat).then(res => res.text().then(dat => window.location.href = "/editor/" + dat))
-    }) 
+        }).then(dat => dat).then(res => res.text().then(dat => {
+                                                    setUrl("/editor/" + dat)
+                                                    setGotUrl(true)
+                                                }))
+    }
 
-    return(<h1>Home</h1>)
+    return(
+    <>
+    <Button positive onClick={clickHandler}>Click here to generate a new Link!</Button>
+    {gotUrl &&
+    <a href={url}>Click here to go the the generate link!</a>}
+    </>
+    )
 }
 export default Home;
